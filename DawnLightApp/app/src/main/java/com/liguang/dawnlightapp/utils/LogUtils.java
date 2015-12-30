61b84d2a395e5b6ff348475af71b4ea5a8b875ea
Zang Liguang <zangliguang@baidu.com>
2015-12-30 22:04:50 +0800

    添加几个utils

--- DawnLightApp/app/src/main/java/com/liguang/dawnlightapp/utils/LogUtils.java
+++ DawnLightApp/app/src/main/java/com/liguang/dawnlightapp/utils/LogUtils.java
@@ -3,32 +3,53 @@ package com.liguang.dawnlightapp.utils;
 
 import android.util.Log;
 
-import com.liguang.dawnlightapp.constants.LocalConstants;
-
-import java.io.PrintWriter;
-import java.io.StringWriter;
-
 /**
  * Created by DawnLight on 15/10/19.
  */
 public class LogUtils {
-    static String logLabel = "DawnLight";
-    public static void DebugerTest(String msg) {
-        if(LocalConstants.DEBUGE){
-            Log.v(logLabel, msg);
-        }
-    }
-    public static void DebugerError(String msg,Throwable error) {
-        if(LocalConstants.DEBUGE){
-            Log.v(logLabel, msg + " Exception: " + getExceptionMsg(error));
-        }
+
+    public static boolean isDebug = true;// 是否需要打印bug，可以在application的onCreate函数里面初始化
+    private static final String TAG = "DawnLight";
+
+    // 下面四个是默认tag的函数
+    public static void i(String msg) {
+        if (isDebug)
+            Log.i(TAG, msg);
     }
 
-    private static String getExceptionMsg(Throwable e) {
-        StringWriter sw = new StringWriter(1024);
-        PrintWriter pw = new PrintWriter(sw);
-        e.printStackTrace(pw);
-        pw.close();
-        return sw.toString();
+    public static void d(String msg) {
+        if (isDebug)
+            Log.d(TAG, msg);
+    }
+
+    public static void e(String msg) {
+        if (isDebug)
+            Log.e(TAG, msg);
+    }
+
+    public static void v(String msg) {
+        if (isDebug)
+            Log.v(TAG, msg);
+    }
+
+    // 下面是传入自定义tag的函数
+    public static void i(String tag, String msg) {
+        if (isDebug)
+            Log.i(tag, msg);
+    }
+
+    public static void d(String tag, String msg) {
+        if (isDebug)
+            Log.i(tag, msg);
+    }
+
+    public static void e(String tag, String msg) {
+        if (isDebug)
+            Log.i(tag, msg);
+    }
+
+    public static void v(String tag, String msg) {
+        if (isDebug)
+            Log.i(tag, msg);
     }
 }
